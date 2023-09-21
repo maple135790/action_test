b69c934adf59a4dbac65a63e9ff1db3baf7969e7
Kenneth Hung <kenneth.h@yappap.com>
2023-09-21 12:32:06 +0800

    version: 1.0.6+3

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World 6+2 !'),
+          child: Text('Hello World 6+3 !'),
         ),
       ),
     );
